7616904d52363da59c61fefaba1d6c68ca0af496
Donna Yau <47676379+dyau172@users.noreply.github.com>
2022-11-29 21:51:47 +0000

    add input boxes

--- contact.js
+++ contact.js
@@ -18,14 +18,45 @@ const nameLabel = document.createElement('label'); //creates a label for the nam
 nameLabel.innerHTML = 'Name:';
 createForm.appendChild(nameLabel);
 
+const inputName = document.createElement('input');
+inputName.setAttribute('type', 'text');
+inputName.setAttribute('name', 'dname');
+createForm.appendChild(inputName);
+
 const emailLabel = document.createElement('label'); 
 emailLabel.innerHTML = 'Email:';
 createForm.appendChild(emailLabel);
+
+const inputEmail = document.createElement('input');
+inputEmail.setAttribute('type', 'text');
+inputEmail.setAttribute('name', 'demail');
+createForm.appendChild(inputEmail);
 
 const phoneLabel = document.createElement('label'); 
 phoneLabel.innerHTML = 'Phone:';
 createForm.appendChild(phoneLabel);
 
+const inputPhone = document.createElement('input');
+inputPhone.setAttribute('type', 'text');
+inputPhone.setAttribute('name', 'dphone');
+createForm.appendChild(inputPhone);
+
 const messageLabel = document.createElement('label'); 
 messageLabel.innerHTML = 'Message:';
 createForm.appendChild(messageLabel);
+
+const inputMessage = document.createElement('input');
+inputMessage.setAttribute('type', 'text');
+inputMessage.setAttribute('name', 'dmessage');
+createForm.appendChild(inputMessage);
+
+createForm.appendChild(lineBreak);
+
+const submitButton = document.createElement('button');
+createForm.appendChild(submitButton);
+
+
+function submitMessage(){
+    const subMessage = prompt('Message Sent');
+    alert(subMessage);
+}
